88609ecab5f64c596a1b93393dee9933e643f4f0
Rob-Exp <153669022+Rob-Exp@users.noreply.github.com>
2023-12-13 19:08:00 +0100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized
+
+# **Objectives:**
+# - List popular languages for Data Science
+# - List commonly used libraries used by Data Scientists 
+# - List development environments open source tools used in Data Science
+# - Create an arithmetic expression where multiplication and addition are used
+# - Create an arithmetic expression that converts hours to minutes
+# 
+# ## Author
+# Roberto Esposito
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. MATLAB
+# 5. Scala
+# 6. Julia
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. Numpy
+# 3. TensorFlow
+# 4. scikit-learn
+# 5. Pytorch
+# 6. NLKT (Natural Language Toolkit)
+# 7. Matplotlib
+
+# |Data Science Tools|
+# | ---------------- |
+# | Jupyter Notebook |
+# | RStudio          |
+# | Colab            |
+# | Apache Zeppelin  |
+# | Spyder           |
+
+# In[2]:
+
+
+# This is a simple arithmetic expression to multiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+200/60
+
+
+# In[ ]:
+
+
+
+
